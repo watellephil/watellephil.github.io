c09e56d0de24db738d0f76db9d81ca1bcbb2c44b
watellephil <watellep@gmail.com>
2025-10-20 20:52:32 -0400

    tentative 5 resolution bug

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+import Index from "./pages/Index.tsx";
+import NotFound from "./pages/NotFound.tsx";
 import Velo from "./pages/Velo.tsx";
 
 const queryClient = new QueryClient();
