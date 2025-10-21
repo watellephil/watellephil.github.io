2d30194c6cca43daa2424507b4e7eea4fbf427c9
watellephil <watellep@gmail.com>
2025-10-20 20:45:51 -0400

    tentative 4 resolution bug

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Velo from "./pages/Velo";
+import Velo from "./pages/Velo.tsx";
 
 const queryClient = new QueryClient();
 
