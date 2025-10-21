89204aa29a577f6d487f57efc3fad76858186430
watellephil <watellep@gmail.com>
2025-10-20 20:53:19 -0400

    tentative 5 resolution de bugs

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,12 +1,11 @@
-import Navigation from "@/components/Navigation";
-import Hero from "@/components/Hero";
-import About from "@/components/About";
-import Skills from "@/components/Skills";
-import Community from "@/components/Community";
-import Projects from "@/components/Projects";
-import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
-
+import Navigation from "@/components/Navigation.tsx";
+import Hero from "@/components/Hero.tsx";
+import About from "@/components/About.tsx";
+import Skills from "@/components/Skills.tsx";
+import Community from "@/components/Community.tsx";
+import Projects from "@/components/Projects.tsx";
+import Contact from "@/components/Contact.tsx";
+import Footer from "@/components/Footer.tsx";
 const Index = () => {
   return (
     <div className="min-h-screen">
