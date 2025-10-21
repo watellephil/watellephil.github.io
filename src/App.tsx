33577bb4e306980ffe46ff0dcdd7669feba8a07e
watellephil <watellep@gmail.com>
2025-10-20 20:55:53 -0400

    tentative 6 et je crois la dernière de résolution de bug

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
-import Velo from "./pages/Velo.tsx";
+import Velo from "./pages/velo.tsx";
 
 const queryClient = new QueryClient();
 
